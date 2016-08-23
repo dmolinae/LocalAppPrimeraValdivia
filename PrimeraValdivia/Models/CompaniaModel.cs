fc1de24512a85469475ceadc575fcab5c95a396b
danie <danie@DESKTOP-ODVE527>
2016-08-23 16:08:45 -0300

    Comenzando con MVVM

---
+++ PrimeraValdivia/Models/CompaniaModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeraValdivia.Models
+{
+    class CompaniaModel
+    {
+        public int idCompania
+        {
+            get;
+            set;
+        } 
+        public string rut
+        {
+            get;
+            set;
+        }
+        public string clave
+        {
+            get;
+            set;
+        }
+        public string calle
+        {
+            get;
+            set;
+        }
+        public string numeroCalle
+        {
+            get;
+            set;
+        }
+        public string ciudad
+        {
+            get;
+            set;
+        }
+        public string nombreCompania
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ PrimeraValdivia/ViewModels/CompaniaViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PrimeraValdivia.Models;
+using System.Collections.ObjectModel;
+
+namespace PrimeraValdivia.ViewModels
+{
+    class CompaniaViewModel : ViewModelBase
+    {
+        private CompaniaModel _compania;
+
+        public CompaniaModel Compania
+        {
+            get
+            {
+                return this._compania;
+            }
+            set
+            {
+                this._compania = value;
+                OnPropertyChanged();
+            }
+        }
+        private ObservableCollection<CompaniaModel> _companias;
+
+        public ObservableCollection<CompaniaModel> Companias
+        {
+            get
+            {
+                if (this._companias == null)
+                    this._companias = new ObservableCollection<CompaniaModel>();
+                return this._companias;
+            }
+            set
+            {
+                this._companias = value;
+                OnPropertyChanged();
+            }
+        }
+        public CompaniaViewModel()
+        {
+            this.Companias.Add(new CompaniaModel()
+            {
+                idCompania = 0,
+                rut = "18578070-8",
+                clave = "1234", 
+                calle = "General Lagos",
+                numeroCalle = "1618",
+                ciudad = "Valdivia",
+                nombreCompania = "Los penes voladores"
+            });
+        }
+    }
+}
