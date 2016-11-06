17527a3ccd1c1e20293cc105cd204ac44220b1e6
Daniel Molina <dmolinae@outlook.com>
2016-11-05 23:30:40 -0300

    Generacion de codigo para modelo a partir de diagrama UML

--- PrimeraValdivia/Models/Apoyo.cs
+++ PrimeraValdivia/Models/Apoyo.cs
@@ -1,6 +1,11 @@
-﻿using PrimeraValdivia.ViewModels;
+﻿
+
+using PrimeraValdivia.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,95 +14,205 @@ namespace PrimeraValdivia.Models
 {
     class Apoyo : ViewModelBase
     {
-        private int _idApoyo;
-        public int idApoyo
+        private Utils utils = new Utils();
+        private string query;
+
+        #region Atributos
+        
+		private int _idApoyo;
+		public int idApoyo
+		{
+			get { return _idApoyo; }
+			set
+			{
+				_idApoyo = value;
+				OnPropertyChanged("idApoyo");
+			}
+		}
+
+		private String _tipo;
+		public String tipo
+		{
+			get { return _tipo; }
+			set
+			{
+				_tipo = value;
+				OnPropertyChanged("tipo");
+			}
+		}
+
+		private String _procedencia;
+		public String procedencia
+		{
+			get { return _procedencia; }
+			set
+			{
+				_procedencia = value;
+				OnPropertyChanged("procedencia");
+			}
+		}
+
+		private String _personaCargo;
+		public String personaCargo
+		{
+			get { return _personaCargo; }
+			set
+			{
+				_personaCargo = value;
+				OnPropertyChanged("personaCargo");
+			}
+		}
+
+		private String _rango;
+		public String rango
+		{
+			get { return _rango; }
+			set
+			{
+				_rango = value;
+				OnPropertyChanged("rango");
+			}
+		}
+
+		private String _patente;
+		public String patente
+		{
+			get { return _patente; }
+			set
+			{
+				_patente = value;
+				OnPropertyChanged("patente");
+			}
+		}
+
+		private String _compania;
+		public String compania
+		{
+			get { return _compania; }
+			set
+			{
+				_compania = value;
+				OnPropertyChanged("compania");
+			}
+		}
+
+		private String _municipalidad;
+		public String municipalidad
+		{
+			get { return _municipalidad; }
+			set
+			{
+				_municipalidad = value;
+				OnPropertyChanged("municipalidad");
+			}
+		}
+
+		private int _fk_idEventoApoyo;
+		public int fk_idEventoApoyo
+		{
+			get { return _fk_idEventoApoyo; }
+			set
+			{
+				_fk_idEventoApoyo = value;
+				OnPropertyChanged("fk_idEventoApoyo");
+			}
+		}
+
+
+        #endregion
+
+        #region Metodos
+
+        public Apoyo()
         {
-            get { return _idApoyo; }
-            set
-            {
-                _idApoyo = value;
-                OnPropertyChanged("idApoyo");
-            }
+
         }
-        private string _tipo;
-        public string tipo
-        {
-            get { return _tipo; }
-            set
-            {
-                _tipo = value;
-                OnPropertyChanged("tipo");
-            }
-        }
-        private string _procedencia;
-        public string procedencia
-        {
-            get { return _procedencia; }
-            set
-            {
-                _procedencia = value;
-                OnPropertyChanged("procedencia");
-            }
-        }
-        private string _personaCargo;
-        public string personaCargo
-        {
-            get { return _personaCargo; }
-            set
-            {
-                _personaCargo = value;
-                OnPropertyChanged("personaCargo");
-            }
-        }
-        private string _rango;
-        public string rango
-        {
-            get { return _rango; }
-            set
-            {
-                _rango = value;
-                OnPropertyChanged("rango");
-            }
-        }
-        private string _patente;
-        public string patente
-        {
-            get { return _patente; }
-            set
-            {
-                _patente = value;
-                OnPropertyChanged("patente");
-            }
-        }
-        private string _compania;
-        public string compania
-        {
-            get { return _compania; }
-            set
-            {
-                _compania = value;
-                OnPropertyChanged("compania");
-            }
-        }
-        private string _municipalidad;
-        public string municipalidad
-        {
-            get { return _municipalidad; }
-            set
-            {
-                _municipalidad = value;
-                OnPropertyChanged("municipalidad");
-            }
-        }
-        private int _fk_idEventoApoyo;
-        public int fk_idEventoApoyo
-        {
-            get { return _fk_idEventoApoyo; }
-            set
-            {
-                _fk_idEventoApoyo = value;
-                OnPropertyChanged("fk_idEventoApoyo");
-            }
-        }
+
+        public Apoyo(int idApoyo, String tipo, String procedencia, String personaCargo, String rango, String patente, String compania, String municipalidad, int fk_idEventoApoyo)
+		{
+			this.idApoyo = idApoyo;
+			this.tipo = tipo;
+			this.procedencia = procedencia;
+			this.personaCargo = personaCargo;
+			this.rango = rango;
+			this.patente = patente;
+			this.compania = compania;
+			this.municipalidad = municipalidad;
+			this.fk_idEventoApoyo = fk_idEventoApoyo;
+		}
+
+        public void AgregarApoyo(Apoyo Apoyo)
+		{
+			query = String.Format(
+				"INSERT INTO Apoyo(idApoyo,tipo,procedencia,personaCargo,rango,patente,compania,municipalidad,fk_idEventoApoyo) VALUES({0},'{1}','{2}','{3}','{4}','{5}','{6}','{7}',{8})",
+				Apoyo.idApoyo,
+				Apoyo.tipo,
+				Apoyo.procedencia,
+				Apoyo.personaCargo,
+				Apoyo.rango,
+				Apoyo.patente,
+				Apoyo.compania,
+				Apoyo.municipalidad,
+				Apoyo.fk_idEventoApoyo
+				);
+			utils.ExecuteNonQuery(query);
+		}
+
+        public void EditarApoyo(Apoyo Apoyo, int idApoyo)
+		{
+			query = String.Format(
+				"UPDATE Apoyo SET idApoyo = {0}, tipo = '{1}', procedencia = '{2}', personaCargo = '{3}', rango = '{4}', patente = '{5}', compania = '{6}', municipalidad = '{7}', fk_idEventoApoyo = {8} WHERE idApoyo = {9}",
+				Apoyo.idApoyo,
+				Apoyo.tipo,
+				Apoyo.procedencia,
+				Apoyo.personaCargo,
+				Apoyo.rango,
+				Apoyo.patente,
+				Apoyo.compania,
+				Apoyo.municipalidad,
+				Apoyo.fk_idEventoApoyo,
+				idApoyo
+				);
+			utils.ExecuteNonQuery(query);
+		}
+
+        public ObservableCollection<Apoyo> ObtenerApoyos()
+		{
+			ObservableCollection<Apoyo> Apoyos = new ObservableCollection<Apoyo>();
+			query = " SELECT * FROM Apoyo";
+			DataTable dt = utils.ExecuteQuery(query);
+			foreach (DataRow row in dt.Rows)
+			{
+				Apoyo Apoyo = new Apoyo(
+					int.Parse(row["idApoyo"].ToString()),
+					row["tipo"].ToString(),
+					row["procedencia"].ToString(),
+					row["personaCargo"].ToString(),
+					row["rango"].ToString(),
+					row["patente"].ToString(),
+					row["compania"].ToString(),
+					row["municipalidad"].ToString(),
+					int.Parse(row["fk_idEventoApoyo"].ToString())
+				);
+				Apoyos.Add(Apoyo);
+			}
+			return Apoyos;
+		}
+
+        public void IniciarId()
+		{
+			query = "SELECT count(*) FROM Apoyo";
+			DataTable dt = utils.ExecuteQuery(query);
+			foreach (DataRow row in dt.Rows)
+			{
+				this.idApoyo = int.Parse(row["idApoyo"].ToString());
+			}
+		}
+        #endregion
     }
 }
+
+
+
+
+
---
+++ PrimeraValdivia/Models/Material.cs
@@ -0,0 +1,143 @@
+﻿
+
+using PrimeraValdivia.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeraValdivia.Models
+{
+    class Material : ViewModelBase
+    {
+        private Utils utils = new Utils();
+        private string query;
+
+        #region Atributos
+        
+		private int _idMaterial;
+		public int idMaterial
+		{
+			get { return _idMaterial; }
+			set
+			{
+				_idMaterial = value;
+				OnPropertyChanged("idMaterial");
+			}
+		}
+
+		private String _nombre;
+		public String nombre
+		{
+			get { return _nombre; }
+			set
+			{
+				_nombre = value;
+				OnPropertyChanged("nombre");
+			}
+		}
+
+		private String _descripcion;
+		public String descripcion
+		{
+			get { return _descripcion; }
+			set
+			{
+				_descripcion = value;
+				OnPropertyChanged("descripcion");
+			}
+		}
+
+		private int _fk_idCarro;
+		public int fk_idCarro
+		{
+			get { return _fk_idCarro; }
+			set
+			{
+				_fk_idCarro = value;
+				OnPropertyChanged("fk_idCarro");
+			}
+		}
+
+
+        #endregion
+
+        #region Metodos
+
+        public Material()
+        {
+
+        }
+
+        public Material(int idMaterial, String nombre, String descripcion, int fk_idCarro)
+		{
+			this.idMaterial = idMaterial;
+			this.nombre = nombre;
+			this.descripcion = descripcion;
+			this.fk_idCarro = fk_idCarro;
+		}
+
+        public void AgregarMaterial(Material Material)
+		{
+			query = String.Format(
+				"INSERT INTO Material(idMaterial,nombre,descripcion,fk_idCarro) VALUES({0},'{1}','{2}',{3})",
+				Material.idMaterial,
+				Material.nombre,
+				Material.descripcion,
+				Material.fk_idCarro
+				);
+			utils.ExecuteNonQuery(query);
+		}
+
+        public void EditarMaterial(Material Material, int idMaterial)
+		{
+			query = String.Format(
+				"UPDATE Material SET idMaterial = {0}, nombre = '{1}', descripcion = '{2}', fk_idCarro = {3} WHERE idMaterial = {4}",
+				Material.idMaterial,
+				Material.nombre,
+				Material.descripcion,
+				Material.fk_idCarro,
+				idMaterial
+				);
+			utils.ExecuteNonQuery(query);
+		}
+
+        public ObservableCollection<Material> ObtenerMaterials()
+		{
+			ObservableCollection<Material> Materials = new ObservableCollection<Material>();
+			query = " SELECT * FROM Material";
+			DataTable dt = utils.ExecuteQuery(query);
+			foreach (DataRow row in dt.Rows)
+			{
+				Material Material = new Material(
+					int.Parse(row["idMaterial"].ToString()),
+					row["nombre"].ToString(),
+					row["descripcion"].ToString(),
+					int.Parse(row["fk_idCarro"].ToString())
+				);
+				Materials.Add(Material);
+			}
+			return Materials;
+		}
+
+        public void IniciarId()
+		{
+			query = "SELECT count(*) FROM Material";
+			DataTable dt = utils.ExecuteQuery(query);
+			foreach (DataRow row in dt.Rows)
+			{
+				this.idMaterial = int.Parse(row["idMaterial"].ToString());
+			}
+		}
+        #endregion
+    }
+}
+
+
+
+
+
